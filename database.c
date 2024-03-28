51f93607fb78eb1a566adca17f8cb8c30e246f1a
Doa <156461653+doaaltay@users.noreply.github.com>
2024-03-28 17:39:34 +0000

    hardcoded additional data type

--- database.c
+++ database.c
@@ -15,6 +15,7 @@
 #define maxavailability 15
 #define MAX_DATA 100
 #define MAX_USER_DATABASE 1000
+#define MAX_COMPANY_DATABASE 1000
 
 
 void add();
@@ -28,6 +29,17 @@ void help();
 void addhelp();
 char* field_to_string(int field);
 int num_users = 0;
+
+
+int num_companies=0;
+
+void addcompany();
+void companies();
+void searchcompany(char* tokens[], int tokens_length);
+void updatecompany(char* tokens[4]);
+void companytypes(); //types to update
+void deletecompany(char* tokens[2]);
+void addcompanyhelp();
 
 FILE *users_binary;
 
@@ -46,7 +58,27 @@ typedef struct{
     char availability[MAX_DATA]; //person's availability 
 } User;
 
+typedef struct{
+    #define maxspecies 20
+    #define maxheight 4
+    char species[maxspecies];
+    char age[maxage];
+    char height [maxheight];
+    
+}Tree;
+
+typedef struct {
+    char name[maxname];
+    char phone[maxphone];
+    char email[maxemail];
+    char founded[maxbirthday];
+    char location[maxcity];
+    char industry[maxjob];
+    char hiring[maxavailability];
+} Company;
+
 User userDatabase[MAX_USER_DATABASE];
+Company companyDatabase[MAX_USER_DATABASE];
 
 //header structure
 typedef struct {
@@ -63,6 +95,26 @@ User deserialize_user(FILE *file) {
     User user;
     fread(&user, sizeof(User), 1, file);
     return user;
+}
+
+void serialize_tree(Tree tree, FILE *file) {
+    fwrite(&tree, sizeof(Tree), 1, file);
+}
+
+Tree deserialize_tree(FILE *file) {
+    Tree tree;
+    fread(&tree, sizeof(Tree), 1, file);
+    return tree;
+}
+
+void serialize_company(Company company, FILE *file) {
+    fwrite(&company, sizeof(Company), 1, file);
+}
+
+Company deserialize_company(FILE *file) {
+    Company company;
+    fread(&company, sizeof(Company), 1, file);
+    return company;
 }
 
 void write_to_binary_file(User *userDatabase, int num_users) {
@@ -102,7 +154,40 @@ void read_from_binary_file(User *userDatabase, int *num_users) {
     fclose(file);
 }
 
+void write_to_binary_file_company(Company *companyDatabase, int num_companies) {
+    FILE *file = fopen("companies.bin", "wb");
+    if (file == NULL) {
+        printf("Could not open file for writing\n");
+        return;
+    }
 
+    Header header = {num_companies, sizeof(Company), "Company"};
+    fwrite(&header, sizeof(Header), 1, file);
+
+    for(int i = 0; i < num_companies; i++) {
+        serialize_company(companyDatabase[i], file);
+    }
+
+    fclose(file);
+}
+
+void read_from_binary_file_company(Company *companyDatabase, int *num_companies) {
+    FILE *file = fopen("companies.bin", "rb");
+    if (file == NULL) {
+        printf("Could not open file for reading\n");
+        return;
+    }
+
+    Header header;
+    fread(&header, sizeof(Header), 1, file);
+    *num_companies = header.num_records;
+
+    for(int i = 0; i < header.num_records; i++) {
+        companyDatabase[i] = deserialize_company(file);
+    }
+
+    fclose(file);
+}
 
 void add(char* user_info){
     if(num_users >= MAX_USER_DATABASE){
@@ -145,6 +230,40 @@ void add(char* user_info){
     userDatabase[num_users++] = new_user;
     write_to_binary_file(userDatabase, num_users);
 }   
+void addcompany(char* company_info){
+    if(num_companies >= MAX_COMPANY_DATABASE){
+        printf("Database is full\n");
+        return;
+    }   
+
+    Company new_company={0}; //initialize all fields to 0
+    memset(&new_company, 0, sizeof(Company)); // clear the struct
+    char* token;
+    char* rest=company_info;
+
+    int field = 0;
+    for(field = 0; field < 7; field++) {
+        char formatted[MAX_DATA];
+        int max_size;
+        token = strsep(&rest, ",");
+        if (token == NULL) {
+            token = "";
+        }
+        switch (field) {
+            case 0: max_size = maxname; strncpy(new_company.name, token, maxname); break;
+
+            case 1: max_size = maxphone; strncpy(new_company.phone, token, maxphone); break;
+            case 2: max_size = maxemail; strncpy(new_company.email, token, maxemail); break;
+            case 3: max_size = maxbirthday; strncpy(new_company.founded, token, maxbirthday); break;
+            case 4: max_size = maxcity; strncpy(new_company.location, token, maxcity); break;
+            case 5: max_size = maxjob; strncpy(new_company.industry, token, maxjob); break;
+            case 6: max_size = maxavailability; strncpy(new_company.hiring, token, maxavailability); break;
+            default: max_size = MAX_DATA; break;
+        }
+    }
+    companyDatabase[num_companies++] = new_company;
+    write_to_binary_file_company(companyDatabase, num_companies);
+}
 void update(char* tokens[]) {
     char* phone_numb = tokens[1];
     char* type = tokens[2];
@@ -198,6 +317,52 @@ void update(char* tokens[]) {
    
     write_to_binary_file(userDatabase, num_users);
 }
+
+void updatecompany(char* tokens[]) {
+    char* company_phone = tokens[1];
+    char* type = tokens[2];
+    char* change = tokens[3];
+
+    Company companyDatabase[MAX_COMPANY_DATABASE];
+    int num_companies = 0;
+
+    read_from_binary_file_company(companyDatabase, &num_companies);
+
+    int found = 0;
+
+    for(int i = 0; i < num_companies; i++) {
+        if(strcmp(companyDatabase[i].phone, company_phone) == 0) {
+            found = 1;
+            printf("Matched company phone\n");
+
+            if(strcmp(type, "name") == 0) {
+                strncpy(companyDatabase[i].name, change, maxname);
+            } else if(strcmp(type, "phone") == 0) {
+                strncpy(companyDatabase[i].phone, change, maxphone);
+            } else if(strcmp(type, "founded") == 0) {
+                strncpy(companyDatabase[i].founded, change, maxbirthday);
+            } else if(strcmp(type, "email") == 0) {
+                strncpy(companyDatabase[i].email, change, maxemail);
+            } else if(strcmp(type, "location") == 0) {
+                strncpy(companyDatabase[i].location, change, maxcity);
+            } else if(strcmp(type, "industry") == 0) {
+                strncpy(companyDatabase[i].industry, change, maxjob);
+            } else if(strcmp(type, "hiring") == 0) {
+                strncpy(companyDatabase[i].hiring, change, maxavailability);
+            }
+
+            printf("Updated field %s\n", type);
+            break;
+        }
+    }
+
+    if (!found) {
+        printf("Company phone not found\n");
+        return;
+    }
+
+    write_to_binary_file_company(companyDatabase, num_companies);
+}
 void main() {
     printf("Welcome to the networking system!\n");
 
@@ -207,15 +372,20 @@ void main() {
     "\nUsage:\n"
     "  execute <command> [options]\n\n"
     "Commands:\n"
-    "  add                                   Add a new user.\n"
-    "  addhelp                               Show the format to add a new user.\n"
-    "  search <phone number>                 Search their name by entering the phone number.\n"
-    "  update <phone number> <type> <change> Update <type> by phone number. Enter type to see types you can change.\n"
-    "  delete <phone number>                 Delete user info by phone number.\n"
-    "  users                                 List all users.\n"
-    "  types                                 List types used in update.\n"
-    "  help                                  Show this list of commands.\n"
-    "  exit                                  Close this program and save updates.\n\n";
+    "  adduser                                   Add a new user.\n"
+    "  adduserhelp                               Show the format to add a new user.\n"
+    "  searchuser <phone number>                 Search their name by entering the phone number.\n"
+    "  updateuser <phone number> <type> <change> Update <type> by phone number. Enter types to see types you can change.\n"
+    "  deleteuser <phone number>                 Delete user info by phone number.\n"
+    "  users                                     List all users.\n"
+    "  usertypes                                 List types used in update.\n"
+    "  addcompany                                Add a new company.\n"
+    "  addcompanyhelp                            Show the format to add a new company.\n"
+    "  searchcompany <name>                      Search the company by entering the name.\n"
+    "  updatecompany <phone> <type> <change>     Update <type> by company phone. Enter companytypes to see types you can change.\n"
+    "  deletecompany <phone>                     Delete company info by phone number.\n"
+    "  help                                      Show this list of commands.\n"
+    "  exit                                      Close this program and save updates.\n\n";
     printf("%s", cmds);
     
     char command[100];
@@ -239,7 +409,7 @@ void main() {
         }
         char *cmd = tokens[0];
 
-        if (strcmp(cmd, "add") == 0) {
+        if (strcmp(cmd, "adduser") == 0) {
             if(tokens_length < 2){
                 printf("Invalid number of arguments\n");
                 printf("Usage: add <user info>\n");
@@ -247,28 +417,61 @@ void main() {
             }
             add(tokens[1]);
         } 
-            else if(strcmp(cmd, "addhelp") == 0){
+            else if(strcmp(cmd, "adduserhelp") == 0){
             addhelp();
         }
-        else if(strcmp(cmd, "search")== 0){
+        else if(strcmp(cmd, "searchuser")== 0){
             search(tokens, tokens_length);
         }
         else if (strcmp(cmd, "users")==0){
             users();
         }
 
-        else if(strcmp(cmd,"types")==0){
+        else if(strcmp(cmd,"usertypes")==0){
             types();
         }
 
-        else if(strcmp(cmd,"update")==0){
+        else if(strcmp(cmd,"updateuser")==0){
             update(tokens);
         }
 
-        else if(strcmp(cmd,"delete")==0){
+        else if(strcmp(cmd,"deleteuser")==0){
             delete(tokens);
         }
+        else if (strcmp(cmd,"addcompany")==0){
+                if(tokens_length < 2){
+                printf("Invalid number of arguments\n");
+                printf("Usage: add <user info>\n");
+                continue;
+            }
+            addcompany(tokens[1]);
+        }
 
+        else if (strcmp(cmd,"addcompanyhelp")==0){
+            addcompanyhelp();
+        }
+
+        else if(strcmp(cmd,"searchcompany")==0){
+            searchcompany(tokens, tokens_length);
+        }
+
+        else if(strcmp(cmd,"updatecompany")==0){
+            updatecompany(tokens);
+        }
+
+        else if(strcmp(cmd,"deletecompany")==0){
+            deletecompany(tokens);
+        }
+
+        else if(strcmp(cmd,"companies")==0){
+            companies();
+        }
+
+        else if(strcmp(cmd,"companytypes")==0){
+            companytypes();
+        }
+
+        
         else if(strcmp(cmd,"help")==0){
             printf("%s", cmds);
         }
@@ -285,6 +488,13 @@ void main() {
     }
 
     printf("Changes saved, thank you for using the network system!\n");
+}
+
+void addcompanyhelp(){
+    printf("Enter company information in the following format:\n\n");
+    printf("name,phone,email,founded,location,industry,hiring\n\n");
+    printf("To skip information, enter two consecutive commas\n");
+    printf("Example: Google,1234567890\n");
 }
 
 
@@ -370,6 +580,44 @@ void search (char* tokens[], int tokens_length){
     fclose(file);
 }
 
+void searchcompany(char* tokens[], int tokens_length){
+    char* company_name = tokens[1];
+
+    FILE *file = fopen("companies.bin", "rb");
+    if(file == NULL){
+        printf("Could not open file for reading-search\n");
+        return;
+    }
+
+    Header header;
+    fread(&header, sizeof(Header), 1, file);
+
+    Company company;
+    int match = 0;
+
+    for(int i = 0; i < header.num_records; i++) {
+        company = deserialize_company(file);
+        char* trimmed_name = trim(company.name); // trim the company name
+        if(strcmp(trimmed_name, company_name) == 0){
+            match = 1;
+            printf("Name: %s\n", company.name);
+            printf("Phone: %s\n", company.phone);
+            printf("Email: %s\n", company.email);
+            printf("Founded: %s\n", company.founded);
+            printf("Location: %s\n", company.location);
+            printf("Industry: %s\n", company.industry);
+            printf("Hiring: %s\n", company.hiring);
+            break;
+        }
+    }
+
+    if(!match){
+        printf("No company found with name %s\n", company_name);
+    }
+
+    fclose(file);
+}
+
 void users() {
     User userDatabase[MAX_USER_DATABASE];
     int num_users = 0;
@@ -378,11 +626,23 @@ void users() {
         printf("%s, %s \n", userDatabase[i].name, userDatabase[i].phone);
     }
 }
+void companies() {
+    Company companyDatabase[MAX_COMPANY_DATABASE];
+    int num_companies = 0;
+    read_from_binary_file_company(companyDatabase, &num_companies);
+    for(int i = 0; i < num_companies; i++) {
+        printf("%s, %s \n", companyDatabase[i].name, companyDatabase[i].phone);
+    }
+}
 
 void types(){
     
      printf("name\nemail\nbirthday\nage\njob\ncity\npronouns\npreferred\navailability\n");
 
+}
+
+void companytypes(){
+    printf("name\nphone\nemail\nfounded\nlocation\nindustry\nhiring\n");
 }
 
 void delete(char* tokens[2]) {
@@ -415,4 +675,35 @@ void delete(char* tokens[2]) {
     }
 
     write_to_binary_file(userDatabase, num_users);
+}
+
+void deletecompany(char* tokens[2]) {
+    char* phone_numb = tokens[1];
+    Company companyDatabase[MAX_COMPANY_DATABASE];
+    int num_companies = 0;
+
+    read_from_binary_file_company(companyDatabase, &num_companies);
+
+    int found = 0;
+
+    for(int i = 0; i < num_companies; i++) {
+        if(strcmp(companyDatabase[i].phone, phone_numb) == 0) {
+            found = 1;
+            printf("Matched phone number, deleting company\n");
+
+            for(int j = i; j < num_companies - 1; j++) {
+                companyDatabase[j] = companyDatabase[j + 1];
+            }
+
+            num_companies--;
+            break;
+        }
+    }
+
+    if (!found) {
+        printf("Phone number not found\n");
+        return;
+    }
+
+    write_to_binary_file_company(companyDatabase, num_companies);
 }
